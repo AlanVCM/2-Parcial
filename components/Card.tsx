316fad46f138e1a09912deed0639cbd860236ed7
Alan Choque <alan39.chok@gmail.com>
2025-05-03 09:21:01 -0400

    Creando el organismo

---
+++ components/Card.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import Description from './Description'
+import End from './End'
+
+const Card = () => {
+  return (
+    <div>
+        <Header/>
+        <Description/>
+        <End/>
+    </div>
+  )
+}
+
+export default Card
